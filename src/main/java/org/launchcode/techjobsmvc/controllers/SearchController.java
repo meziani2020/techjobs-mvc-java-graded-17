1c2d3e616e0451a2e9e71ed1f8c1dde82f4fbacf
Mohamed <issine14@gmail.com>
2024-11-21 00:43:52 -0600

    ga3 all test pass

--- src/main/java/org/launchcode/techjobsmvc/controllers/SearchController.java
+++ src/main/java/org/launchcode/techjobsmvc/controllers/SearchController.java
@@ -1,9 +1,12 @@
 package org.launchcode.techjobsmvc.controllers;
 
+import org.launchcode.techjobsmvc.models.Job;
+import org.launchcode.techjobsmvc.models.JobData;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
 
 import static org.launchcode.techjobsmvc.controllers.ListController.columnChoices;
 
@@ -22,6 +25,15 @@ public class SearchController {
     }
 
     // TODO #3 - Create a handler to process a search request and render the updated search view.
-
+    @PostMapping("results")
+  public String  displaySearchResults(Model model,@RequestParam String searchType, @RequestParam String searchTerm){
+        ArrayList<Job> jobList;
+        jobList= JobData.findByColumnAndValue(searchType,searchTerm);
+        model.addAttribute("jobs", jobList);
+        model.addAttribute("title", "Jobs with " + searchType + ": " + searchTerm);
+        model.addAttribute("columns", columnChoices);
+        return "search";
+        //return "search/results"+jobList.size();
+    }
 }
 
